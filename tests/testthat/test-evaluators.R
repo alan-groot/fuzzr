b8f17e393e6513dfe8a71f81e150cac9771fbf9f
Alan Groot <alan.groot@openanalytics.eu>
2024-12-24 08:20:03 +0100

    Add snapshot tests for debugging

---
+++ tests/testthat/test-evaluators.R
@@ -0,0 +1,13 @@
+test_that("snapshot tests from example", {
+  fr <- fuzz_function_call(quote(identity(x = 1)))
+  expect_snapshot(as.data.frame(fr))
+  
+  fr2 <- fuzz_function_call(quote(identical(x = TRUE, y = FALSE)))
+  expect_snapshot(as.data.frame(fr2))
+  
+  fr3 <- fuzz_function_call(quote(dirname(".")))
+  expect_snapshot(as.data.frame(fr3))
+  
+  fr4 <- fuzz_function_call(quote(dirname(path = ".")))
+  expect_snapshot(as.data.frame(fr4))
+})
